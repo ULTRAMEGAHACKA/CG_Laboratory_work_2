ae801fc667357faac75b7c07c7cd2ff49702169f
[enteron] <[w.slleeps@gmail.com]>
2022-04-20 20:57:37 +0500

    Lesson #12 (without snd triangle)

--- CG_Laboratory_work_2.cpp
+++ CG_Laboratory_work_2.cpp
@@ -6,33 +6,66 @@
 #include <iostream>
 #include "PipeLine.h"
 using namespace std;
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 GLuint VBO;
-double Scale = 0.0;
-glm::vec3 Vertices[3];
+GLuint IBO;
+glm::vec3 Vertices[4];
 
 void RenderSceneCB()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    glEnableVertexAttribArray(0);
-    glColor4f(1.0f, 1.0f, 0.0f, 1.0);
-    glDrawArrays(GL_LINE_LOOP, 0, 3); // GL_TRIANGLES // GL_LINE_LOOP // GL_LINE_STRIP
+
+    static float Scale = 0.0f;
     Scale += 0.003;
-    //glDrawArrays(GL_LINE_LOOP, 3, 3);
     
     Pipeline p;
-    p.Scale(sinf(Scale * 0.1f), sinf(Scale * 0.1f), sinf(Scale * 0.1f));
-    p.WorldPos(sinf(Scale), sinf(Scale), 0.0f);
-    p.Rotate(sinf(Scale) * 90.0f, sinf(Scale) * 90.0f, sinf(Scale) * 90.0f);
+    //p.Scale(sinf(Scale * 0.1f), sinf(Scale * 0.1f), sinf(Scale * 0.1f));//sinf(Scale * 0.1f), sinf(Scale * 0.1f), sinf(Scale * 0.1f)
+    p.WorldPos(sinf(Scale), 0.0f, 10.0f);
+    p.Rotate(sinf(Scale) * 90, sinf(Scale) * 90, sinf(Scale) * 90);//sinf(Scale) * 90.0f, sinf(Scale) * 90.0f, sinf(Scale) * 90.0f
+    p.SetPerspectiveProj(30.0f, WINDOW_WIDTH, WINDOW_HEIGHT, 1.0f, 1000.0f);
 
     glLoadMatrixf(reinterpret_cast<const float*>(p.GetTrans()));
 
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glColor4f(1.0f, 1.0f, 0.0f, 1.0);
+    //glDrawArrays(GL_LINE_LOOP, 0, 3); // GL_TRIANGLES // GL_LINE_LOOP // GL_LINE_STRIP
+    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
     glDisableVertexAttribArray(0);
+
     glutSwapBuffers();
     glutIdleFunc(RenderSceneCB);
 }
+static void CreateVertexBuffer()
+{
+    Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.5773f);
+    Vertices[1] = glm::vec3(0.0f, -1.0f, -1.15475);
+    Vertices[2] = glm::vec3(1.0f, -1.0f, 0.5773f);
+    Vertices[3] = glm::vec3(0.0f, 1.0f, 0.0f);
 
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+static void CreateIndexBuffer()
+{
+    unsigned int Indices[] = { 0, 3, 1,
+                               1, 3, 2,
+                               2, 3, 0,
+                               0, 2, 1 };
+
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+}
 int main(int argc, char** argv)
 {
 
@@ -50,15 +83,22 @@ int main(int argc, char** argv)
         return 1;
     }
     
-    Vertices[0] = glm::vec3(-0.5f, 0.0f, 0.0f);
+    /*Vertices[0] = glm::vec3(-0.5f, 0.0f, 0.0f);
     Vertices[1] = glm::vec3(0.5f, 0.0f, 0.0f);
     Vertices[2] = glm::vec3(0.0f, 0.5f, 0.0f);
-
+    Vertices[3] = glm::vec3(0.0f, 1.0f, 0.0f);
     glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO); //привязка array buff к указателю VBO
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);*/
+
+    /*Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.5773f);
+    Vertices[1] = glm::vec3(0.0f, -1.0f, -1.15475);
+    Vertices[2] = glm::vec3(1.0f, -1.0f, 0.5773f);
+    Vertices[3] = glm::vec3(0.0f, 1.0f, 0.0f);*/
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    CreateVertexBuffer();
+    CreateIndexBuffer();
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);//
     glEnable(GL_BLEND); //enable alpha blending
--- PipeLine.h
+++ PipeLine.h
@@ -6,12 +6,9 @@
 #include<glm/vec3.hpp>
 #include<glm/mat4x4.hpp>
 #include<glm/gtc/matrix_transform.hpp>
+#include <glm/glm.hpp>
 #include <iostream>
 using namespace std;
-const double M_PI = 3.141592653589793;
-
-#define ToRadian(x) ((x) * M_PI / 180.0f)
-#define ToDegree(x) ((x) * 180.0f / M_PI)
 
 class Pipeline
 {
@@ -43,7 +40,14 @@ public:
         m_rotateInfo.y = RotateY;
         m_rotateInfo.z = RotateZ;
     }
-
+    void SetPerspectiveProj(float FOV, float Width, float Height, float zNear, float zFar)
+    {
+        m_persProj.FOV = FOV;
+        m_persProj.Width = Width;
+        m_persProj.Height = Height;
+        m_persProj.zNear = zNear;
+        m_persProj.zFar = zFar;
+    }
     const glm::mat4* GetTrans() 
     {
         glm::mat4 ScaleTrans(
@@ -58,28 +62,40 @@ public:
             0.0f, 0.0f, 1.0f, 0.0f  ,
             m_worldPos.x, m_worldPos.y, m_worldPos.z, 1.0f); // положение
 
-        const float x = ToRadian(m_rotateInfo.x);
-        const float y = ToRadian(m_rotateInfo.y);
-        const float z = ToRadian(m_rotateInfo.z);
+        const float x = glm::radians(m_rotateInfo.x);
+        const float y = glm::radians(m_rotateInfo.y);
+        const float z = glm::radians(m_rotateInfo.z);
 
-        glm::mat4 transformMatrixZ(
-            cosf(z), sinf(z), 0.0f, 0.0f,
-            -sinf(z), cosf(z), 0.0f, 0.0f,
+        glm::mat4x4 transformMatrixZ(
+            cosf(z), -sinf(z), 0.0f, 0.0f,
+            sinf(z), cosf(z), 0.0f, 0.0f,
             0.0f, 0.0f, 1.0f, 0.0f,
             0.0f, 0.0f, 0.0f, 1.0f); //вокруг Z
-        glm::mat4 transformMatrixY(
-            cosf(y), 0.0f, sinf(y), 0.0f,
-            0.0f, 1.0f, cosf(y), 0.0f,
-            -sinf(y), 0.0f, 0.0f, 0.0f,
+        glm::mat4x4 transformMatrixY(
+            cosf(y), 0.0f, -sinf(x), 0.0f,
+            0.0f, 1.0f, 0.0f, 0.0f,
+            sinf(x), 0.0f, cosf(y), 0.0f,
             0.0f, 0.0f, 0.0f, 1.0f); //вокруг Y
-        glm::mat4 transformMatrixX(
-            1.0f, cosf(x), sinf(x), 0.0f,
-            0.0f, -sinf(x), cosf(x), 0.0f,
-            0.0f, 0.0f, 1.0f, 0.0f,
+        glm::mat4x4 transformMatrixX(
+            1.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, cosf(x), -sinf(x), 0.0f,
+            0.0f, sinf(x), cosf(x), 0.0f,
             0.0f, 0.0f, 0.0f, 1.0f); // вокруг X
-        glm::mat4 RotateTrans = transformMatrixX * transformMatrixY * transformMatrixZ;
+        glm::mat4x4 RotateTrans = transformMatrixZ * transformMatrixY * transformMatrixX;
 
-        glm::mat4 m_transformation = TranslationTrans * ScaleTrans * RotateTrans;
+        const float ar = m_persProj.Width / m_persProj.Height;
+        const float zNear = m_persProj.zNear;
+        const float zFar = m_persProj.zFar;
+        const float zRange = zNear - zFar;
+        const float tanHalfFOV = tanf(glm::radians(m_persProj.FOV / 2.0));
+
+        glm::mat4 PersProjTrans(
+            1.0f / (tanHalfFOV * ar), cosf(x), sinf(x), 0.0f,
+            0.0f, 1.0f / tanHalfFOV, cosf(x), 0.0f,
+            0.0f, 0.0f, (-zNear - zFar) / zRange, 1.0f,
+            0.0f, 0.0f, 2.0f * zFar * zNear / zRange, 0.0f);
+
+        glm::mat4 m_transformation = PersProjTrans * TranslationTrans * RotateTrans * ScaleTrans;
         return &m_transformation;
     }
 
@@ -87,6 +103,14 @@ private:
     glm::vec3 m_scale;
     glm::vec3 m_worldPos;
     glm::vec3 m_rotateInfo;
+
+    struct {
+        float FOV;
+        float Width;
+        float Height;
+        float zNear;
+        float zFar;
+    } m_persProj;
 };
 
 #endif	/* PIPELINE_H */
